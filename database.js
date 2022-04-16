0688714183271f353cad3568b06366f720563313
Ernest Ermongkonchai <erneste@email.unc.edu>
2022-04-15 23:29:25 -0400

    changed print stmts

--- database.js
+++ database.js
@@ -38,7 +38,8 @@ if (row === undefined) {
     // Execute SQL commands that we just wrote above.
     db.exec(sqlInit);
     // Echo information about what we just did to the console.
-    console.log('Your database has been initialized with a new table and two entries containing a username and password.');
+    //console.log('Your database has been initialized with a new table and two entries containing a username and password.');
+    console.log('New Table Added to Database.')
 } else {
     // Since the database already exists, echo that to the console.
     console.log('Database exists.')
